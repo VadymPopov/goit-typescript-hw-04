08b2101b37203f3b8a1429d8e5c20228f04b799f
VadymPopov <boycristal10@gmail.com>
2024-01-20 10:46:57 -0500

    tasks

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,5 +1,16 @@
 import React, {useReducer} from "react";
 
+type RequestStep = 'start' | 'pending' | 'finished' | 'idle';
+
+interface State {
+  isRequestInProgress: boolean;
+  requestStep: RequestStep;
+}
+
+type Action = {
+ type : 'START_REQUEST' | 'PENDING_REQUEST' | 'FINISH_REQUEST' | 'RESET_REQUEST';
+}
+
 const initialState: State = {
   isRequestInProgress: false,
   requestStep: 'idle',
